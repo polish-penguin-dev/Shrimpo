2579d00b773c836bd5fcb2e0ef19c7554d1d13fb
paige <88659700+paigeroid@users.noreply.github.com>
2023-07-02 16:34:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
 /*
   Shrimpo🍤
   Make chatbots quickly, and easily. Uses levenshtein distance to compare strings and categorizes them into specific intents (works similarly to Google's Dialogflow).
-  Made by Aleksander Wegrzyn (penguins184 discord).
+  Made by Aleksander Wegrzyn (@penguins184 discord).
+  Class made by Paige (@paigeroid discord).
 */
 
-const TrainingData = {};
-let DefaultPhrase = "Sorry, I didn't understand. Can you please rephrase your question?"
+
 const stringSimilarity = require("string-similarity");
+const def = "Sorry, I didn't understand. Can you please rephrase your question?";
 
-function train(intent, trainingphrases, trainingresponses) {
-  TrainingData[intent] = {
-    trainingphrases: trainingphrases.map(phrase => phrase.toLowerCase()),
-    trainingresponses
-  };
+
+class Shrimpo {
+	constructor(defaults=def) {
+		this.defaults = defaults;
+		this.trainingData = {};
+	}
+
+	train(intent, trainingphrases, trainingresponses) {
+		this.trainingData[intent] = {
+			trainingphrases: trainingphrases.map(phrase => phrase.toLowerCase()),
+			trainingresponses
+		};
+	}
+
+	ask(prompt) {
+		const promptLowercase = prompt.toLowerCase();
+		let closestMatch = null;
+		let highestScore = 0;
+
+		for (const intent in this.trainingData) {
+			const trainingPhrases = this.trainingData[intent].trainingphrases;
+
+			const { bestMatch } = stringSimilarity.findBestMatch(promptLowercase, trainingPhrases);
+
+			if (bestMatch.rating > highestScore) {
+				highestScore = bestMatch.rating;
+				closestMatch = intent;
+			}
+		}
+
+		
+		if (closestMatch) {
+			const trainingResponses = this.trainingData[closestMatch].trainingresponses;
+			const randomResponse =
+			trainingResponses[Math.floor(Math.random() * trainingResponses.length)];
+			return randomResponse;
+		}
+
+		
+		if (Array.isArray(this.defaults)) {
+			return this.defaults[Math.floor(Math.random() * this.defaults.length)];
+		} else {
+			return this.defaults;
+		}
+	}
 }
 
-function ask(prompt) {
-  const promptLowercase = prompt.toLowerCase();
-  let closestMatch = null;
-  let highestScore = 0;
 
-  for (const intent in TrainingData) {
-    const trainingPhrases = TrainingData[intent].trainingphrases;
-
-    const { bestMatch } = stringSimilarity.findBestMatch(promptLowercase, trainingPhrases);
-
-    if (bestMatch.rating > highestScore) {
-      highestScore = bestMatch.rating;
-      closestMatch = intent;
-    }
-  }
-
-  if (closestMatch) {
-    const trainingResponses = TrainingData[closestMatch].trainingresponses;
-    const randomResponse =
-      trainingResponses[Math.floor(Math.random() * trainingResponses.length)];
-    return randomResponse;
-  }
-
-  if(Array.isArray(DefaultPhrase)) {
-    return DefaultPhrase[Math.floor(Math.random() * DefaultPhrase.length)];
-  } else {
-    return DefaultPhrase;
-  }
-}
-
-function setDefaultPhrase(x) {
-  DefaultPhrase = x;
-}
-
-module.exports = { train, ask, setDefaultPhrase };
+module.exports = Shrimpo;
